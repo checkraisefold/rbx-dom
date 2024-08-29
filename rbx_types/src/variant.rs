b6c4e53644bd917496f8a448f42041d537bc1695
checkraisefold <checkraisefold@crf.gay>
2024-08-28 20:27:28 -0700

    Initial support
    Spec file, begin integrating it
    
    whoops
    
    fixes this ugly bit
    
    reword
    
    flat_map flat_map map
    
    make nested for loops great again
    
    The linter has spoken
    
    requested changes
    
    Spec file improvements, encoder improvements
    
    Reword
    
    Reword (2)
    
    consistency
    
    link water occupancy to shorelines
    
    chunk max const
    
    comments
    
    current changes
    
    docstring
    
    naming convention

--- rbx_types/src/variant.rs
+++ rbx_types/src/variant.rs
@@ -2,7 +2,7 @@ use crate::{
     Attributes, Axes, BinaryString, BrickColor, CFrame, Color3, Color3uint8, ColorSequence,
     Content, ContentId, Enum, EnumItem, Faces, Font, MaterialColors, NumberRange, NumberSequence,
     PhysicalProperties, Ray, Rect, Ref, Region3, Region3int16, SecurityCapabilities, SharedString,
-    Tags, UDim, UDim2, UniqueId, Vector2, Vector2int16, Vector3, Vector3int16,
+    SmoothGrid, Tags, UDim, UDim2, UniqueId, Vector2, Vector2int16, Vector3, Vector3int16,
 };
 
 /// Reduces boilerplate from listing different values of Variant by wrapping
@@ -129,8 +129,8 @@ make_variant! {
     UniqueId(UniqueId),
     MaterialColors(MaterialColors),
     SecurityCapabilities(SecurityCapabilities),
-    EnumItem(EnumItem),
     Content(Content),
+    Terrain(Terrain),
 }
 
 impl From<&'_ str> for Variant {
